7a9975f09367dd137b7933462b0f023042545821
lethi-nks <56311990+lethi-nks@users.noreply.github.com>
2021-02-22 14:30:42 +0200

    Databse updated

--- giftbox_db.sql
+++ giftbox_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 17, 2021 at 07:26 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.0
+-- Generation Time: Feb 22, 2021 at 01:29 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -68,6 +68,14 @@ CREATE TABLE `customer` (
   `admin_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`cust_id`, `firstname`, `lastname`, `email`, `password`, `admin_id`) VALUES
+(101, 'lethu', 'nkosi', 'lethu@gmail.com', 'Lethu@123', 5432),
+(102, 'lee', 'nks', 'nks@gmail.com', 'nks@1234', 5432);
+
 -- --------------------------------------------------------
 
 --
@@ -80,7 +88,7 @@ CREATE TABLE `item` (
   `item_price` int(10) NOT NULL,
   `size` varchar(50) DEFAULT NULL,
   `title` varchar(50) DEFAULT NULL,
-  `image` blob DEFAULT NULL
+  `image` varchar(250) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -88,10 +96,6 @@ CREATE TABLE `item` (
 --
 
 INSERT INTO `item` (`item_id`, `category`, `item_price`, `size`, `title`, `image`) VALUES
-(1, 'y', 0, '[object Object]', '[object Object]', NULL),
-(2, '[object Object]', 0, '[object Object]', '[object Object]', NULL),
-(3, '[object Object]', 0, '[object Object]', '[object Object]', NULL),
-(4, '[object Object]', 0, '[object Object]', '[object Object]', NULL),
 (5, 'birthday', 30, 'small', 'blackbox', NULL),
 (6, 'anniversary', 60, 'small', 'rosegoldbox', NULL);
 
@@ -107,6 +111,20 @@ CREATE TABLE `order` (
   `address` varchar(100) NOT NULL,
   `cust_id` int(11) NOT NULL,
   `item_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_tbl`
+--
+
+CREATE TABLE `order_tbl` (
+  `order_id` int(11) NOT NULL,
+  `order_date` date NOT NULL DEFAULT current_timestamp(),
+  `cust_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `address` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -188,6 +206,14 @@ ALTER TABLE `order`
   ADD KEY `item_id` (`item_id`);
 
 --
+-- Indexes for table `order_tbl`
+--
+ALTER TABLE `order_tbl`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `item_id` (`item_id`),
+  ADD KEY `cust_id` (`cust_id`);
+
+--
 -- Indexes for table `payment`
 --
 ALTER TABLE `payment`
@@ -230,7 +256,7 @@ ALTER TABLE `cart`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `cust_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cust_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=104;
 
 --
 -- AUTO_INCREMENT for table `item`
@@ -242,6 +268,12 @@ ALTER TABLE `item`
 -- AUTO_INCREMENT for table `order`
 --
 ALTER TABLE `order`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order_tbl`
+--
+ALTER TABLE `order_tbl`
   MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -261,6 +293,42 @@ ALTER TABLE `report`
 --
 ALTER TABLE `wishlist`
   MODIFY `wish_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `customer`
+--
+ALTER TABLE `customer`
+  ADD CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`admin_id`);
+
+--
+-- Constraints for table `order_tbl`
+--
+ALTER TABLE `order_tbl`
+  ADD CONSTRAINT `order_tbl_ibfk_1` FOREIGN KEY (`cust_id`) REFERENCES `customer` (`cust_id`),
+  ADD CONSTRAINT `order_tbl_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`);
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`admin_id`),
+  ADD CONSTRAINT `payment_ibfk_2` FOREIGN KEY (`cust_id`) REFERENCES `customer` (`cust_id`);
+
+--
+-- Constraints for table `report`
+--
+ALTER TABLE `report`
+  ADD CONSTRAINT `report_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`admin_id`);
+
+--
+-- Constraints for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  ADD CONSTRAINT `wishlist_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`admin_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
